e402cba86d686eec8c830047e2429b5d88b168a1
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2021-11-28 22:11:00 -0300

    desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: { $and: [{ birthYear: { $ne: null } }, { birthYear: { $ne: " " } }] },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project: {
+      menorAnoNascimento: { $toInt: "$menorAnoNascimento" },
+      _id: 0,
+      maiorAnoNascimento: 1,
+    },
+  },
+]);
